9fb27542c1c70f0bf3b6a365aa946efcf615c412
CCGLP <carlosgolfer@gmail.com>
2017-07-14 00:52:56 +0200

    First Commit to the project.
    Support to create new maps within the editor.

---
+++ 2DProceduralLinesMap/Assets/Editor/ProceduralPropertyDrawer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ProceduralMap))]
+public class ProceduralPropertyDrawer : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        ProceduralMap map = (ProceduralMap)target; 
+        if (GUILayout.Button("Generate"))
+        {
+            map.RemoveMap();
+            map.InitiliazeAndGenerateMap(); 
+        }
+
+    }
+
+}
